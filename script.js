9cd3490032f415e16609927d64aac3d20ac7ba53
Jyothi Prakash <jyothi.prakash@chronus.com>
2018-12-15 19:37:52 +0530

    Scriptjs updated for recommended mentors

--- script.js
+++ script.js
@@ -1,22 +1,28 @@
 // Code goes here
 $(document).ready(function() {
-  $.ajax({
-    type: "GET",
-    url: "https://iitm.localhost.com:3000/p/p1/recommended_mentors.json",
-    crossDomain: true,
-    success: function(json){
-      console.log(json);
-      var response = '',
-            indicator = '';
-        for(var i=0; i < json.results.length; i++){
-          response += '<div class="item"><div><center><img alt="Profilepicture" title="New Hire" id="" width="20%" height="20%" class="table-bordered thick-border img-circle" src="' + json.results[i].picture_url + '" ></center></div><div class=""><center><h3>' + json.results[i].mentor_name + '</h3><p>' + json.results[i].profile_summary + '</p><h5>' +  json.results[i].match_score.toString() + '% match</h5></center></div></div>';
-          indicator += '<li data-target="#myCarousel" data-slide-to="'+i+'"></li>';
-        }
-        $('#homepageItems').append(response);
-        $('#indicators').append(indicator);
-        $('.item').first().addClass('active');
-        $('.carousel-indicators > li').first().addClass('active');
-        $("#myCarousel").carousel();
-    }
-  });
+  chrome.storage.sync.get(['program_url', 'user_id'], function(result) {
+   user_id = parseInt(result.user_id)
+   url = result.program_url
+   if(user_id){ 
+    $.ajax({
+      type: "GET",
+      data: { 'user_id': user_id },
+      url: url + "/recommended_mentors.json",
+      crossDomain: true,
+      success: function(json){
+        console.log(json);
+        var response = '',
+              indicator = '';
+          for(var i=0; i < json.results.length; i++){
+            response += '<div class="item"><div><center><img alt="Profilepicture" title="New Hire" id="" width="20%" height="20%" class="table-bordered thick-border img-circle" src="' + json.results[i].picture_url + '" ></center></div><div class=""><center><h3>' + json.results[i].mentor_name + '</h3><p>' + json.results[i].profile_summary + '</p><h5>' +  json.results[i].match_score.toString() + '% match</h5></center></div></div>';
+            indicator += '<li data-target="#myCarousel" data-slide-to="'+i+'"></li>';
+          }
+          $('#homepageItems').append(response);
+          $('#indicators').append(indicator);
+          $('.item').first().addClass('active');
+          $('.carousel-indicators > li').first().addClass('active');
+          $("#myCarousel").carousel();
+      }
+    });
+  }});
 });
